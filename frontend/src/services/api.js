ad35dc1559314ac8ff1cb487f448eff23a20f6d0
Zaki Mujawar <skyscraperadv1@gmail.com>
2026-09-14 13:27:26 +0530

    Connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://expense-manager-0ew3.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
